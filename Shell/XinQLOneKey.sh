7b625692c98a83fe764eae124698457c09039a97
RiverCrab2021 <85817162+RiverCrab2021@users.noreply.github.com>
2022-07-22 12:14:13 +0800

    Update XinQLOneKey.sh

--- Shell/XinQLOneKey.sh
+++ Shell/XinQLOneKey.sh
@@ -2,17 +2,5 @@
 echo
 "当前node版本(如果没有node，请自行安装): "
 node -v
-echo
-"当前npm版本(如果没有npm，请自行安装): "
-npm -v
-npm config set registry https://registry.npmmirror.com
-cd /QL
-pnpm add -g pnpm
-pnpm install -g
-npm install -g npm dayjs
-echo
-TIME g "依赖安装完毕...建议重启 Docker "
-echo
-TIME g "有任何问题，请在此仓库提交Issue： https://github.com/FlechazoPh/QLDependency"
-echo
+
 exit 0
